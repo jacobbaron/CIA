d2f1fe6eae41993f4ea080a46b9621eaceb7e354
jacobbaron <jbaron@physics.harvard.edu>
2018-01-30 10:57:05 -0500

    multi mix version of this

--- compute_odor_conc.m
+++ compute_odor_conc.m
@@ -1,26 +1,15 @@
-function [odor, conc, inds]=compute_odor_conc(odorID,odor_inf)
-   odor_list=odor_inf.odor_list;
-   odor_concentration_list=odor_inf.odor_concentration_list;
-   odor_colormap=odor_inf.odor_colormap;
-   if odorID==0
-        odor = 'Water';
-        conc = ' ';
-        inds=[NaN,NaN];
+function [odorStrs]=compute_odor_conc(odorSeq,t)
+    idx = odorSeq.odorSeqStep(t);
+   if idx==0
+        odorStrs = {'Water'};
+        
    else
-       if odorID==length(odor_concentration_list)*length(odor_list)
-           ind_odor=length(odor_list);
-           ind_conc=length(odor_concentration_list);
-       else
-           
-        ind_odor = floor(odorID/length(odor_concentration_list))+1;
-        ind_conc = rem(odorID, length(odor_concentration_list));
-        if ind_conc ==0 % added Sep 24, fix bug when rem==0
-            ind_conc = length(odor_concentration_list);
-        end
-       end
-        odor = odor_list{ind_odor};
-
-        conc= odor_concentration_list{ind_conc};
-        inds=[ind_odor,ind_conc];
+       
+       odorIdx = odorSeq.seqArr(:,idx);
+       numOdors = sum(double(odorIdx));
+       odorStrs = cellfun(@(x,y)[x,' ',y],odorSeq.concs(odorIdx),odorSeq.odors(odorIdx),...
+           'UniformOutput',false);
+       odorStrs = [num2str(numOdors),' odors';odorStrs];
+       
    end
 end
