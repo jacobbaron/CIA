9b64042d683df3bd4c6df34abb96389636eadc58
jacobbaron <jbaron@physics.harvard.edu>
2018-02-26 18:24:22 -0500

    memory optimizations

--- HDF5/get_all_volumes.m
+++ HDF5/get_all_volumes.m
@@ -11,34 +11,34 @@ function [img,time]=get_all_volumes(fname,lasers,piezo,img_idx,res,volumes,t,img
         %offset=prod(res)*(frames-1)+1;
         %start=[1,offset(1)];
         %count=[1,prod(res)*length(];
-        
+        tic
         data=h5read(fname,'/img');
 
         unique_lasers=find(any(lasers,1));
         
-        data3d=reshape(data,res(1),res(2),length(img_idx));
-        
+        data=reshape(data,res(1),res(2),length(img_idx));
         which_lasers=zeros(size(lasers,1),1);
         for ii=1:length(lasers)
-            tmp=find(lasers(ii,:));
+            tmp=find(lasers(ii,:), 1);
             if ~isempty(tmp)
                 which_lasers(ii)=find(lasers(ii,:));
             end
         end
         
         [~,~,which_laser_idx]=unique(which_lasers(use_frames));
-        data3d_use=data3d(:,:,use_frames);
+        data=data(:,:,use_frames);
         int_piezo_use=int_piezo(use_frames);
         volumes=volumes(use_frames);
         for ii=1:length(which_laser_idx)
             img(:,:,int_piezo_use(ii),volumes(ii),which_laser_idx(ii))=...
-                data3d_use(:,:,ii);
+                data(:,:,ii)';
         end
         unique_volumes=unique(volumes(volumes>0));
         for ii=1:length(unique_volumes)
            time(ii)=mean(t(volumes==unique_volumes(ii)));
             
         end
-        img = flip(flip(permute(img,[2,1,3,4,5]),1),3);
+        img = flip(flip(img,1),3);
+        toc
         
     
