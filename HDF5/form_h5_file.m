8a8f98c262d4763bc9d52a1e298f0fcc591e5f0d
jacobbaron <jbaron@physics.harvard.edu>
2018-03-07 17:29:33 -0500

    add some stuff

---
+++ HDF5/form_h5_file.m
@@ -0,0 +1,56 @@
+function [redFiltImg] = form_h5_file(filename)
+
+[piezo,volumes,lasers,t,img_idx,res,...
+    acq_start_time]=LoadImgProperties(filename);
+piezo=piezo(1:length(img_idx));
+volumes=volumes(1:length(img_idx));
+lasers=lasers(1:length(img_idx),:);
+image_times=zeros(max(volumes),1);
+for ii=1:max(volumes)
+    image_times(ii)=mean(t(volumes==ii));
+end
+
+which_lasers=find(any(lasers,1));
+zstack_pos=20*(piezo(volumes==1 & lasers(:,which_lasers(1))));
+zdiff_size=diff(zstack_pos);
+
+unique_lasers=find(any(lasers,1));
+    num_lasers=length(find(any(lasers,1)));    
+    
+    for ii=1:size(lasers,2)
+        zdepth(ii)=length(find(lasers(volumes==1,ii)));
+    end
+
+
+movSize = double([res(1),res(2),max(zdepth),length(unique(volumes(volumes>0)))]);
+
+start_time_str=h5readatt(filename,'/','Start Time');
+
+[pth,newFname] = fileparts(filename);
+%%
+mkdir(newFname);
+redFname = fullfile(pth,newFname,[newFname,'_red.h5']);
+%redFiltName = fullfile(pth,newFname,[newFname,'_redHPfilt.h5']);
+greenFname = fullfile(pth,newFname,[newFname,'_green.h5']);
+deetFname =  fullfile(pth,newFname,[newFname,'_deets.h5']);
+h5create(redFname ,'/redImg',movSize,'ChunkSize',[movSize(1:3),1],'Datatype','uint16');
+h5create(greenFname ,'/greenImg',movSize,'ChunkSize',[movSize(1:3),1],'Datatype','uint16');
+%h5create(redFiltName ,'/redFiltImg',movSize,'ChunkSize',[movSize(1:3),1],'Datatype','single');
+%h=waitbar(0,'Transforming and saving...');
+parfor_progress(length(image_times));
+redFiltImg = zeros(movSize([1,2,4]),'single');
+for ii=1:length(image_times)
+    [greenImg,redImg] = get_single_volume(filename,volumes,lasers,piezo,img_idx,res,ii);
+    filt1 = medfilt2(max(redImg,[],3));
+    redFiltImg(:,:,ii) = filt1-imgaussfilt(filt1,3);
+    h5write(redFname, '/redImg',redImg,[1,1,1,ii],[movSize(1:3),1]);
+    h5write(greenFname, '/greenImg',greenImg,[1,1,1,ii],[movSize(1:3),1]);
+    %h5write(redFiltName, '/redFiltImg',redFiltImg,[1,1,1,ii],[movSize(1:3),1]);
+%    fprintf('iter %d/%d\n',ii,length(image_times))
+end
+parfor_progress(0);
+%close(h)
+
+h5create(deetFname,'/t',length(image_times));
+h5write(deetFname,'/t',image_times);
+h5writeatt(deetFname,'/','Start Time',start_time_str{1});
---
+++ HDF5/get_single_volume.m
@@ -0,0 +1,34 @@
+function [greenImg,redImg]=get_single_volume(fname,volumes,lasers,piezo,img_idx,res,volume)
+%output index: (i,j,k), green is c=1, red is c=2   
+
+%vol_mem=img(:,:,:,volume,:);
+    %if all(vol_mem(:)==0) %data not yet loaded
+        [~,~,int_piezo]=unique(piezo);
+        %volumes_data=volumes(img_idx);
+        frames=find(volumes==volume);
+       
+        offset=prod(res)*(frames-1)+1;
+        start=[1,offset(1)];
+        count=[1,prod(res)*length(frames)];
+        
+        data=(h5read(fname,'/img',start,count));
+
+        unique_lasers=find(any(lasers,1));
+        
+        data3d=reshape(data,res(1),res(2),length(frames));       
+        redImg = zeros(res(1),res(2),length(find(lasers(frames,2))),'uint16');
+        greenImg = zeros(res(1),res(2),length(find(lasers(frames,2))),'uint16');
+        for ii=1:length(unique_lasers)
+            laser_id=find(lasers(frames,unique_lasers(ii)));
+            [~,idx]=sort(piezo(frames(laser_id)));
+            if ii==1
+                greenImg(:,:,int_piezo(laser_id(idx)))=...
+                    flip(flip(permute(data3d(:,:,laser_id(idx)),[2,1,3]),1),3);
+            else
+                redImg(:,:,int_piezo(laser_id(idx)))=...
+                    flip(flip(permute(data3d(:,:,laser_id(idx)),[2,1,3]),1),3);
+            end
+        end
+        
+        
+   % end
