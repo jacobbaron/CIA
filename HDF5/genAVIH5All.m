702a433186b76e9a1ba6bcab0887115d65af7928
jacobbaron <jbaron@physics.harvard.edu>
2018-03-22 15:07:48 -0400

    avi stuff

--- HDF5/genAVIH5All.m
+++ HDF5/genAVIH5All.m
@@ -1,7 +1,15 @@
 filez = dir('*.h5');
 
+
+
 for ii=1:length(filez)
-   genAviH5(filez(ii).name);
+    [~,nm] = fileparts(filez(ii).name);
+    
+    if ~(exist([nm,'.avi'],'file')==2)
+        fprintf('Converting file %s...',filez(ii).name);
+        genAviH5(filez(ii).name);
+        fprintf('done\n');
+    end
     
     
 end
