fea212314f294142bcd7082a67dfab8ea511043b
jacobbaron <jbaron@physics.harvard.edu>
2018-04-23 10:13:18 -0400

    memory saver

--- HDF5/get_volume.m
+++ HDF5/get_volume.m
@@ -13,11 +13,11 @@ function [img]=get_volume(fname,volume,lasers,piezo,img_idx,res,volumes,img)
 
         unique_lasers=find(any(lasers,1));
         
-        data3d=reshape(data,res(1),res(2),length(frames));
+        data=reshape(data,res(1),res(2),length(frames));
         
         for ii=1:length(unique_lasers)
             laser_id=find(lasers(frames,unique_lasers(ii)));    
             [~,idx]=sort(piezo(frames(laser_id)));
-            img(:,:,int_piezo(laser_id(idx)),volume,ii)=data3d(:,:,laser_id(idx));            
+            img(:,:,int_piezo(laser_id(idx)),volume,ii)=data(:,:,laser_id(idx));            
         end
     end
